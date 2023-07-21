507e0362a7ac8491e485d930af2314eeabe95e80
Prathibha Ratnayake <prsthibha@gmail.com>
2023-07-21 13:32:53 +0530

    Add favorites page

---
+++ app/actions/getFavouriteListings.ts
@@ -0,0 +1,27 @@
+import prisma from '@/lib/prismaDb';
+import getCurrentUser from './getCurrentUser';
+
+export default async function getFavoriteListings() {
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) return [];
+
+    const favorites = await prisma.listing.findMany({
+      where: {
+        id: {
+          in: [...(currentUser.favoritesId || [])],
+        },
+      },
+    });
+
+    const safeFavorites = favorites.map((favorite) => ({
+      ...favorite,
+      createdAt: favorite.createdAt?.toISOString(),
+    }));
+
+    return safeFavorites;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -64,7 +64,10 @@ const UserMenu: FC<UserMenuProps> = ({ currentUser }) => {
                     onClick={() => router.push('/trips')}
                     label='My trips'
                   />
-                  <MenuItem onClick={() => {}} label='My favourites' />
+                  <MenuItem
+                    onClick={() => router.push('/favorites')}
+                    label='My favourites'
+                  />
                   <MenuItem
                     onClick={() => router.push('/reservations')}
                     label='My reservations'
